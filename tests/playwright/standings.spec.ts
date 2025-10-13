a9ea0423d97374bc1571dccc0e3f249a592385e9
Kenz <mabauer@ualberta.ca>
2025-10-13 13:54:00 -0600

    added conference standings tests

--- tests/playwright/standings.spec.ts
+++ tests/playwright/standings.spec.ts
@@ -1,7 +1,57 @@
 import { test, expect } from '@playwright/test'
+import { hasUncaughtExceptionCaptureCallback } from 'process';
 
-test.describe('home page',  () => {
+test.describe('standings',  () => {
+    
+    // constants for standings tests
+    const expectedAtlanticTeams = [
+            'Florida Panthers', 
+            'Boston Bruins', 
+            'Toronto Maple Leafs', 
+            'Ottawa Senators', 
+            'Montréal Canadiens', 
+            'Tampa Bay Lightning', 
+            'Detroit Red Wings', 
+            'Buffalo Sabres'
+        ].sort();
 
+    const expectedPacificTeams = [
+        'Edmonton Oilers',
+        'Calgary Flames',
+        'Seattle Kraken',
+        'Los Angeles Kings',
+        'Vegas Golden Knights',
+        'Vancouver Canucks',
+        'Anaheim Ducks',
+        'San Jose Sharks'
+    ].sort();
+
+    const expectedCentralTeams = [
+        'Colorado Avalanche',
+        'Dallas Stars',
+        'Nashville Predators',
+        'Minnesota Wild',
+        'Winnipeg Jets',
+        'St. Louis Blues',
+        'Utah Mammoth',
+        'Chicago Blackhawks'
+    ].sort();
+
+    const expectedMetroTeams = [
+        'Carolina Hurricanes',
+        'Washington Capitals',
+        'Pittsburgh Penguins',
+        'New York Rangers',
+        'Columbus Blue Jackets',
+        'New Jersey Devils',
+        'Philadelphia Flyers',
+        'New York Islanders'
+    ].sort();
+
+    const westernConferenceTeams = (expectedCentralTeams.concat(expectedPacificTeams)).sort();
+    const easternConferenceTeams = (expectedMetroTeams.concat(expectedAtlanticTeams)).sort();
+
+    // tests 
     test('division standings grid renders', async ({ page }) => {
         await page.goto('/');
 
@@ -28,24 +78,68 @@ test.describe('home page',  () => {
     test('division standings show correct teams', async ({ page }) => {
         await page.goto('/');
 
-        // get teams in each div
+        // get teams in atlantic
         const atlantic = page.locator('.division-table', { hasText: 'Atlantic' });
-        const teamCells = atlantic.locator('tbody tr td:nth-child(2)');
-        const teamNames = (await teamCells.allTextContents()).map(name => name.trim()).sort();
+        const atlanticTeamCells = atlantic.locator('tbody tr td:nth-child(2)');
+        const atlanticTeams = (await atlanticTeamCells.allTextContents()).map(name => name.trim()).sort();
+
+        // get team in pacific
+        const pacific = page.locator('.division-table', { hasText: 'Pacific' });
+        const pacificTeamCells = pacific.locator('tbody tr td:nth-child(2)');
+        const pacificTeams = (await pacificTeamCells.allTextContents()).map(name => name.trim()).sort();
+
+        // get team in metropolitan
+        const metropolitan = page.locator('.division-table', { hasText: 'Metropolitan' });
+        const metroTeamCells = metropolitan.locator('tbody tr td:nth-child(2)');
+        const metroTeams = (await metroTeamCells.allTextContents()).map(name => name.trim()).sort();
+
+        // get team in central
+        const central = page.locator('.division-table', { hasText: 'Central' });
+        const centralTeamCells = central.locator('tbody tr td:nth-child(2)');
+        const centralTeams = (await centralTeamCells.allTextContents()).map(name => name.trim()).sort();
 
         // check if lists contain the div teams 
-        const expectedAtlanticTeams = [
-            'Florida Panthers', 
-            'Boston Bruins', 
-            'Toronto Maple Leafs', 
-            'Ottawa Senators', 
-            'Montréal Canadiens', 
-            'Tampa Bay Lightning', 
-            'Detroit Red Wings', 
-            'Buffalo Sabres'
-        ].sort();
+        expect(atlanticTeams).toEqual(expectedAtlanticTeams);
+        expect(pacificTeams).toEqual(expectedPacificTeams);
+        expect(metroTeams).toEqual(expectedMetroTeams);
+        expect(centralTeams).toEqual(expectedCentralTeams);
+    });
 
-        expect(teamNames).toEqual(expectedAtlanticTeams);
+    // conference standings
+    test('conference standings western default is correct', async ({ page }) => {
+        await page.goto('/');
+
+        // check the default on page load
+        const selectedOptionValue = await page.getByRole('combobox').inputValue();
+        expect(selectedOptionValue).toBe('Western');
+
+        // get teams conference
+        const conference = page.locator('#conference_standings table');
+        const conferenceTeamCells = conference.locator('tbody tr td:nth-child(2)');
+        const conferenceTeams = (await conferenceTeamCells.allTextContents()).map(name => name.trim()).sort();
+        
+        // check western teams are correct
+        expect(conferenceTeams).toEqual(westernConferenceTeams);        
+    });
+
+    test('conference standings eastern are correct', async ({ page }) => {
+        await page.goto('/');
+
+        // check option selection changes
+        await page.getByRole('combobox').selectOption('Eastern');
+        const selectedOptionValue = await page.getByRole('combobox').inputValue();
+        expect(selectedOptionValue).toBe('Eastern');
+
+        // wait for the table to populate teams
+        await page.locator('#conference_standings table td', { hasText: 'Toronto Maple Leafs' }).waitFor();
+
+        // get teams conference
+        const conference = page.locator('#conference_standings table');
+        const conferenceTeamCells = conference.locator('tbody tr td:nth-child(2)');
+        const conferenceTeams = (await conferenceTeamCells.allTextContents()).map(name => name.trim()).sort();
+        
+        // check eastern teams are correct
+        expect(conferenceTeams).toEqual(easternConferenceTeams);        
     });
 
 });
